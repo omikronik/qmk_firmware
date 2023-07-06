4e1e46a79d609de224c4c85ba52e0a4be2838477
omikronik <yasirceltik9@hotmail.com>
2023-07-06 20:59:21 +0100

    fix keymap

--- keyboards/totem/keymaps/omikronik/keymap.c
+++ keyboards/totem/keymaps/omikronik/keymap.c
@@ -79,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  //╷         ╷         ╷         ╷         ╷         ╷         ╷╷         ╷         ╷         ╷         ╷         ╷         ╷
               KC_1,     KC_2,     KC_3 ,    KC_4,     KC_5,      KC_6 ,    KC_7 ,    KC_8 ,    KC_9 ,    KC_0,
               KC_HASH,  KC_PERC,  KC_LCBR,  KC_RCBR,  KC_QUOT,   KC_PLUS,  KC_MINUS, KC_EQL,   KC_QUES,  KC_COLN,
-    _______,  KC_EXLM,  KC_AT,    KC_LPRN,  KC_LPRN,  KC_DQT,    KC_ASTR,  KC_AMPR,  KC_UNDS,  KC_BSLS,  KC_PIPE,  _______,
+    _______,  KC_EXLM,  KC_AT,    KC_LPRN,  KC_RPRN,  KC_DQT,    KC_ASTR,  KC_AMPR,  KC_UNDS,  KC_BSLS,  KC_PIPE,  _______,
                                   _______,  _______,  _______,   _______,   _______,  _______
  ),
 
